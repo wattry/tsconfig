86e73ed201b934ee58475b1c7f4f0877c6c25903
wattry <09wattry@gmail.com>
2025-01-26 17:18:15 -0800

    Fix vitest configuration

--- vitest.config.js
+++ vitest.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   base: './',
@@ -12,4 +12,4 @@ export default defineConfig({
       reporter: ['text', 'json', 'html']
     }
   },
-})
+});

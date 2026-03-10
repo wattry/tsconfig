e983acab3452aaa1036decfeb51867ffca17dadf
wattry <09wattry@gmail.com>
2026-03-09 21:54:58 -0700

    Fix typescript and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,113 +1,114 @@
-import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import stylistic from '@stylistic/eslint-plugin';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-const files = ['src/**/*.ts', 'types/**/*.ts'];
+const files = ['**/*.ts'];
 const ignores = ['dist/**/*', 'node_modules/**/*'];
 
-export default tseslint.config(
-  { ignores },
+export default defineConfig(
   {
     files,
     extends: [
-      eslint.configs.recommended,
+      { ignores },
       tseslint.configs.recommended,
-    ],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: { ...globals.node },
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    rules: {
-      '@stylistic/arrow-parens': ['error', 'always'],
-      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-      '@stylistic/comma-dangle': [
-        'error',
-        {
-          arrays: 'only-multiline',
-          objects: 'only-multiline',
-          imports: 'only-multiline',
-          exports: 'only-multiline',
-          functions: 'only-multiline',
+      {
+        languageOptions: {
+          ecmaVersion: 'latest',
+          sourceType: 'module',
+          globals: globals.node,
+          parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+          },
         },
-      ],
-      '@stylistic/eol-last': ['error', 'always'],
-      '@stylistic/function-call-argument-newline': 'off',
-      '@stylistic/function-call-spacing': ['error', 'never'],
-      '@stylistic/function-paren-newline': ['error', 'consistent'],
-      '@stylistic/implicit-arrow-linebreak': ['error', 'beside'],
-      '@stylistic/indent': ['error', 2, { SwitchCase: 1 }],
-      '@stylistic/lines-between-class-members': [
-        'error',
-        'always',
-        { exceptAfterSingleLine: true },
-      ],
-      '@stylistic/max-len': [
-        'warn',
-        {
-          code: 120,
-          ignoreComments: true,
-          ignoreStrings: true,
-          ignoreTrailingComments: true,
-          ignoreTemplateLiterals: true,
-          ignoreRegExpLiterals: true,
+        plugins: {
+          '@stylistic': stylistic,
         },
-      ],
-      '@stylistic/no-multi-spaces': ['error', { ignoreEOLComments: true }],
-      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
-      '@stylistic/no-tabs': ['error'],
-      '@stylistic/object-curly-newline': ['error', { consistent: true }],
-      '@stylistic/object-curly-spacing': ['error', 'always'],
-      '@stylistic/operator-linebreak': [
-        'error',
-        'after',
-        { overrides: { '=': 'after', '?': 'before', ':': 'before' } },
-      ],
-      '@stylistic/prefer-for-of': 'off',
-      '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
-      '@stylistic/semi': ['error', 'always'],
-      '@stylistic/space-before-blocks': ['error', 'always'],
-      '@stylistic/space-before-function-paren': [
-        'error',
-        { anonymous: 'always', named: 'never', asyncArrow: 'always' },
-      ],
-      '@stylistic/space-in-parens': ['error', 'never'],
-      '@typescript-eslint/consistent-type-imports': ['error', {
-        prefer: 'type-imports',
-        disallowTypeAnnotations: false,
-      }],
-      '@typescript-eslint/naming-convention': [
-        'warn',
-        { selector: 'default', format: ['camelCase'] },
-        { selector: 'import', format: null },
-        { selector: 'variable', format: ['camelCase', 'UPPER_CASE'] },
-        { selector: 'parameter', format: ['camelCase'], leadingUnderscore: 'allow' },
-        { selector: 'function', format: ['camelCase', 'PascalCase'] },
-        { selector: 'objectLiteralProperty', format: ['camelCase', 'PascalCase', 'UPPER_CASE'] },
-        { selector: 'typeLike', format: ['PascalCase'] },
-        { selector: 'enumMember', format: ['UPPER_CASE', 'camelCase'] },
-      ],
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-shadow': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
-      'consistent-return': 'off',
-      'no-await-in-loop': 'warn',
-      'no-nested-ternary': 'off',
-      'no-param-reassign': 'error',
-      'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-      'no-shadow': 'off',
-      'no-underscore-dangle': 'warn',
-      'no-use-before-define': ['error', 'nofunc'],
-      'prefer-const': 'error',
-      'prefer-destructuring': ['error', { array: false, object: false }],
-    },
+        rules: {
+          '@stylistic/arrow-parens': ['error', 'always'],
+          '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+          '@stylistic/comma-dangle': [
+            'error',
+            {
+              arrays: 'only-multiline',
+              objects: 'only-multiline',
+              imports: 'only-multiline',
+              exports: 'only-multiline',
+              functions: 'only-multiline',
+            },
+          ],
+          '@stylistic/eol-last': ['error', 'always'],
+          '@stylistic/function-call-argument-newline': 'off',
+          '@stylistic/function-call-spacing': ['error', 'never'],
+          '@stylistic/function-paren-newline': ['error', 'consistent'],
+          '@stylistic/implicit-arrow-linebreak': ['error', 'beside'],
+          '@stylistic/indent': ['error', 2, { SwitchCase: 1 }],
+          '@stylistic/lines-between-class-members': [
+            'error',
+            'always',
+            { exceptAfterSingleLine: true },
+          ],
+          '@stylistic/max-len': [
+            'warn',
+            {
+              code: 120,
+              ignoreComments: true,
+              ignoreStrings: true,
+              ignoreTrailingComments: true,
+              ignoreTemplateLiterals: true,
+              ignoreRegExpLiterals: true,
+            },
+          ],
+          '@stylistic/no-multi-spaces': ['error', { ignoreEOLComments: true }],
+          '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
+          '@stylistic/no-tabs': ['error'],
+          '@stylistic/object-curly-newline': ['error', { consistent: true }],
+          '@stylistic/object-curly-spacing': ['error', 'always'],
+          '@stylistic/operator-linebreak': [
+            'error',
+            'after',
+            { overrides: { '=': 'after', '?': 'before', ':': 'before' } },
+          ],
+          '@stylistic/prefer-for-of': 'off',
+          '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+          '@stylistic/semi': ['error', 'always'],
+          '@stylistic/space-before-blocks': ['error', 'always'],
+          '@stylistic/space-before-function-paren': [
+            'error',
+            { anonymous: 'always', named: 'never', asyncArrow: 'always' },
+          ],
+          '@stylistic/space-in-parens': ['error', 'never'],
+          '@typescript-eslint/consistent-type-imports': ['error', {
+            prefer: 'type-imports',
+            disallowTypeAnnotations: false,
+          }],
+          '@typescript-eslint/naming-convention': [
+            'warn',
+            { selector: 'default', format: ['camelCase'] },
+            { selector: 'import', format: null },
+            { selector: 'variable', format: ['camelCase', 'UPPER_CASE'] },
+            { selector: 'parameter', format: ['camelCase'], leadingUnderscore: 'allow' },
+            { selector: 'function', format: ['camelCase', 'PascalCase'] },
+            { selector: 'objectLiteralProperty', format: ['camelCase', 'PascalCase', 'UPPER_CASE'] },
+            { selector: 'typeLike', format: ['PascalCase'] },
+            { selector: 'enumMember', format: ['UPPER_CASE', 'camelCase'] },
+          ],
+          '@typescript-eslint/no-floating-promises': 'error',
+          '@typescript-eslint/no-shadow': 'error',
+          '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+          'consistent-return': 'off',
+          'no-await-in-loop': 'warn',
+          'no-nested-ternary': 'off',
+          'no-param-reassign': 'error',
+          'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+          'no-shadow': 'off',
+          'no-underscore-dangle': 'warn',
+          'no-use-before-define': ['error', 'nofunc'],
+          'prefer-const': 'error',
+          'prefer-destructuring': ['error', { array: false, object: false }],
+        },
+      }
+    ]
   },
 );
